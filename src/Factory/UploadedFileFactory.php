138fe5bb0676a0205a0f1640633cb22e4ed51348
Baimurzaev <baimurzaev@gmail.com>
2019-10-06 15:35:58 +0300

    UploadedFileFactory implementation

---
+++ src/Factory/UploadedFileFactory.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Hawk\Psr7\Factory;
+
+use Hawk\Psr7\UploadedFile;
+use InvalidArgumentException;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UploadedFileInterface;
+
+/**
+ * Class UploadedFileFactory
+ * @package Hawk\Psr7\Factory
+ *
+ * @link https://github.com/hawkphp/psr7-http-message GitHub main page
+ */
+class UploadedFileFactory implements UploadedFileFactoryInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createUploadedFile(
+        StreamInterface $stream,
+        int $size = null,
+        int $error = UPLOAD_ERR_OK,
+        string $clientFilename = null,
+        string $clientMediaType = null
+    ): UploadedFileInterface
+    {
+        $file = $stream->getMetadata('uri');
+
+        if (!is_string($file) || !is_readable($file)) {
+            throw new InvalidArgumentException('File is not readable.');
+        }
+
+        if (is_null($size)) {
+            $size = $stream->getSize();
+        }
+
+        return new UploadedFile($file, $clientFilename, $clientMediaType, $size, $error);
+    }
+}
