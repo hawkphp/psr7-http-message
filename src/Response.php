e179bd3be6402235dc1126e13f17a92d5be3ba4e
Baimurzaev <baimurzaev@gmail.com>
2019-10-10 10:54:36 +0300

    Response implementation

---
+++ src/Response.php
@@ -0,0 +1,173 @@
+<?php declare(strict_types=1);
+
+namespace Hawk\Psr7;
+
+use Hawk\Psr7\Factory\StreamFactory;
+use InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+
+/**
+ * Class Response
+ * @package Hawk\Psr7
+ *
+ * @link https://github.com/hawkphp/psr7-http-message GitHub main page
+ */
+class Response extends Message implements ResponseInterface
+{
+
+    /**
+     * @var int
+     */
+    private $statusCode = 200;
+
+    /**
+     * @var string
+     */
+    private $reasonPhrase;
+
+    /**
+     * Map of standard HTTP status code/reason phrases
+     *
+     * @var array
+     */
+    private $phrases = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        103 => 'Early Hints',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Too Early',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'Connection Closed Without Response',
+        451 => 'Unavailable For Legal Reasons',
+        499 => 'Client Closed Request',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        599 => 'Network Connect Timeout Error',
+    ];
+
+    /**
+     * Response constructor.
+     * @param int $code
+     * @param null $headers
+     * @param StreamInterface|null $body
+     */
+    public function __construct(int $code = 200, $headers = null, StreamInterface $body = null)
+    {
+        $this->statusCode = $this->filterStatus($code);
+        $this->headers = $headers ? $headers : new Headers();
+        $this->body = $body ? $body : (new StreamFactory())->createStream();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function withStatus($code, $reasonPhrase = ''): self
+    {
+        $code = $this->filterStatus($code);
+
+        $clone = clone $this;
+        $clone->statusCode = $code;
+
+        if ((null === $reasonPhrase || '' === $reasonPhrase) && isset($this->phrases[$clone->statusCode])) {
+            $reasonPhrase = $this->phrases[$clone->statusCode];
+        }
+
+        $clone->reasonPhrase = $reasonPhrase;
+
+        return $clone;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReasonPhrase(): string
+    {
+        return $this->reasonPhrase;
+    }
+
+
+    /**
+     * Filter HTTP status code
+     * @param $code
+     * @return int
+     */
+    protected function filterStatus($code): int
+    {
+        if (!is_int($code) && !is_string($code)) {
+            throw new InvalidArgumentException('Status code must be an integer value');
+        }
+
+        $code = (int)$code;
+
+        if ($code < 100 || $code > 599) {
+            throw new InvalidArgumentException('Status code must be an integer value between 100 and 599');
+        }
+
+        return $code;
+    }
+
+}
