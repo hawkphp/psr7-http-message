563aa5dbf23dc6210e8e24671281443ffdcc4905
Baimurzaev <baimurzaev@gmail.com>
2019-10-23 03:24:46 +0300

    added headers test

---
+++ tests/HeadersTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace Hawk\Tests\Psr7;
+
+use Hawk\Psr7\Headers;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class HeadersTest
+ * @package Hawk\Tests\Psr7
+ */
+class HeadersTest extends TestCase
+{
+    public function factory()
+    {
+        return new Headers();
+    }
+
+    public function testHeader()
+    {
+        $headers = new Headers(['Accept' => 'application/xml']);
+        $this->assertEquals(['application/xml'], $headers->getHeader('Accept'));
+    }
+
+    public function testGetHeader()
+    {
+        $headers = new Headers();
+        $headers->addHeader('accept', 'text/html');
+        $this->assertEquals(['text/html'], $headers->getHeader('Accept'));
+    }
+
+
+    public function testAddHeader()
+    {
+        $headers = new Headers();
+        $headers->addHeader('Accept', 'text/html');
+        $headers->addHeader('Accept', 'application/xml');
+        $this->assertEquals(['text/html', 'application/xml'], $headers->getHeader('Accept'));
+    }
+
+    public function testSetHeaders()
+    {
+        $headersLine = [
+            'Accept' => ['application/xml', 'text/html'],
+            'Connection' => ['keep-alive']
+        ];
+
+        $headers = new Headers();
+        $headers->setHeaders($headersLine);
+
+        $this->assertEquals($headersLine, $headers->getHeaders());
+    }
+}
