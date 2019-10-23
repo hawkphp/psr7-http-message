00c43437bacbc16f85d6cb7085b8abb876a1835f
Baimurzaev <baimurzaev@gmail.com>
2019-10-23 04:09:01 +0300

    unit test 100% ok

--- tests/HeadersTest.php
+++ tests/HeadersTest.php
@@ -22,20 +22,33 @@ class HeadersTest extends TestCase
         $this->assertEquals(['application/xml'], $headers->getHeader('Accept'));
     }
 
-    public function testGetHeader()
-    {
-        $headers = new Headers();
-        $headers->addHeader('accept', 'text/html');
-        $this->assertEquals(['text/html'], $headers->getHeader('Accept'));
-    }
-
-
     public function testAddHeader()
     {
         $headers = new Headers();
         $headers->addHeader('Accept', 'text/html');
         $headers->addHeader('Accept', 'application/xml');
         $this->assertEquals(['text/html', 'application/xml'], $headers->getHeader('Accept'));
+    }
+
+    public function testGetHeader()
+    {
+        $headers = new Headers();
+        $headers->addHeader('accept', 'text/html');
+        $this->assertEquals(['text/html'], $headers->getHeader('Accept'));
+        $this->assertEquals(['text/html'], $headers->getHeader('accept'));
+    }
+
+    public function testGetHeaders()
+    {
+        $headersLine = [
+            'Accept' => ['application/xml', 'text/html'],
+            'Connection' => ['keep-alive']
+        ];
+
+        $headers = new Headers(['Accept' => 'application/xml']);
+        $headers->addHeader('Accept', 'text/html');
+        $headers->addHeader('Connection', 'keep-alive');
+        $this->assertEquals($headersLine, $headers->getHeaders());
     }
 
     public function testSetHeaders()
@@ -50,4 +63,22 @@ class HeadersTest extends TestCase
 
         $this->assertEquals($headersLine, $headers->getHeaders());
     }
+
+    public function testRemoveHeader()
+    {
+        $headers = new Headers(['Accept' => 'application/xml']);
+        $headers->removeHeader('Accept');
+
+        $this->assertEquals([], $headers->getHeaders());
+        $this->assertEquals([], $headers->getHeader('Accept'));
+    }
+
+    public function testHasHeader()
+    {
+        $headers = new Headers(['Accept' => 'text/html']);
+        $this->assertIsBool($headers->hasHeader('Accept'));
+        $this->assertEquals(true, $headers->hasHeader('Accept'));
+        $this->assertEquals(false, $headers->hasHeader('Connection'));
+    }
+
 }
