756d8d420428042bad7c348714deff54dd5d8357
Baimurzaev <baimurzaev@gmail.com>
2019-10-25 18:23:33 +0000

    implementation message stub

---
+++ tests/Mocks/MessageMock.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Hawk\Tests\Psr7\Mocks;
+
+use Hawk\Psr7\Headers;
+use Hawk\Psr7\Message;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Class MessageMock
+ * @package Hawk\Tests\Psr7\Mocks
+ */
+class MessageMock extends Message
+{
+    /**
+     * @var Headers
+     */
+    public $headers;
+
+    /**
+     * @var StreamInterface
+     */
+    public $body;
+
+    /**
+     * @var string
+     */
+    public $protocolVersion;
+
+}
