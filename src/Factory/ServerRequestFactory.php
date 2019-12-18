9821eb9a4172a99e5edd7eb253c34856f71b6127
Baimurzaev <baimurzaev@gmail.com>
2019-12-18 23:55:15 +0300

    Added fromGlobal method

--- src/Factory/ServerRequestFactory.php
+++ src/Factory/ServerRequestFactory.php
@@ -14,12 +14,12 @@ use Psr\Http\Message\UriInterface;
 class ServerRequestFactory implements ServerRequestFactoryInterface
 {
     /**
-     * @var StreamFactory|StreamFactoryInterface
+     * @var StreamFactory|StreamFactoryInterface|null
      */
     protected $streamFactory;
 
     /**
-     * @var UriFactory|UriFactoryInterface
+     * @var UriFactory|UriFactoryInterface|null
      */
     protected $uriFactory;
 
@@ -52,21 +52,48 @@ class ServerRequestFactory implements ServerRequestFactoryInterface
         }
 
         if (!$uri instanceof UriInterface) {
-            throw new InvalidArgumentException('URI must either be string or instance of ' . UriInterface::class);
+            throw new InvalidArgumentException('URI must either be instance of UriInterface');
         }
+
 
         $body = $this->streamFactory->createStream();
         $headers = new Headers();
+        $cookies = self::parseHeader($headers->getHeader('Cookie', []));
 
         if (!empty($serverParams)) {
-            if (function_exists('getallheaders')) {
-                $headersData = getallheaders();
-                if (is_array($headersData) && count($headersData)) {
-                    $headers->setHeaders($headersData);
+            $headers = Headers::createFromGlobals();
+        }
+
+        return new Request($method, $uri, $body, $headers, $serverParams, $cookies);
+    }
+
+    /**
+     * @param $header
+     * @return array
+     */
+    public static function parseHeader($header): array
+    {
+        if (is_array($header)) {
+            $header = isset($header[0]) ? $header[0] : '';
+        }
+        if (!is_string($header)) {
+            throw new InvalidArgumentException('Cannot parse Cookie data. Header value must be a string.');
+        }
+        $header = rtrim($header, "\r\n");
+        $pieces = preg_split('@[;]\s*@', $header);
+        $cookies = [];
+        if (is_array($pieces)) {
+            foreach ($pieces as $cookie) {
+                $cookie = explode('=', $cookie, 2);
+                if (count($cookie) === 2) {
+                    $key = urldecode($cookie[0]);
+                    $value = urldecode($cookie[1]);
+                    if (!isset($cookies[$key])) {
+                        $cookies[$key] = $value;
+                    }
                 }
             }
         }
-
-        return new Request($uri, $method, $headers, $body);
+        return $cookies;
     }
 }
